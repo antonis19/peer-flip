0d7b717cf8fd126f3cc7c2382c63cb0f4e4dc54a
antonis19 <antonis19@mail.com>
2023-07-07 00:22:39 +0300

    add more STUN and TURN servers

--- peer-flip-frontend/src/Client.ts
+++ peer-flip-frontend/src/Client.ts
@@ -65,15 +65,17 @@ export default class RoomClient {
     private createPeerConnection(targetUserId: string): RTCPeerConnection {
         const peerConnection = new RTCPeerConnection({
             iceServers: [{ urls: 'stun:stun.l.google.com:19302' },
+            { urls: 'stun:stun1.l.google.com:19302' },
+            { urls: 'stun:stun2.l.google.com:19302' },
+            { urls: 'stun:stun3.l.google.com:19302' },
+            { urls: 'stun:stun4.l.google.com:19302' },
             {
                 urls: 'turn:numb.viagenie.ca',
                 credential: 'muazkh',
                 username: 'webrtc@live.com'
             },
             {
-                urls: 'turn:192.158.29.39:3478?transport=udp',
-                credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                username: '28224511:1379330808'
+                urls: 'turn:turn02.hubl.in?transport=tcp',
             },
             {
                 urls: 'turn:192.158.29.39:3478?transport=tcp',
