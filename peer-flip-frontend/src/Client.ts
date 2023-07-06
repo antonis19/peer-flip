bbc7d960bc80eaa0d673362ea80742b91b08742d
antonis19 <antonis19@mail.com>
2023-07-07 00:52:23 +0300

    only use TCP TURN servers

--- peer-flip-frontend/src/Client.ts
+++ peer-flip-frontend/src/Client.ts
@@ -70,19 +70,9 @@ export default class RoomClient {
             { urls: 'stun:stun3.l.google.com:19302' },
             { urls: 'stun:stun4.l.google.com:19302' },
             {
-                urls: 'turn:numb.viagenie.ca',
-                credential: 'muazkh',
-                username: 'webrtc@live.com'
-            },
-            {
                 urls: 'turn:192.158.29.39:3478?transport=tcp',
                 credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
                 username: '28224511:1379330808'
-            },
-            {
-                urls: 'turn:turn.bistri.com:80',
-                credential: 'homeo',
-                username: 'homeo'
             },
             {
                 urls: 'turn:turn.anyfirewall.com:443?transport=tcp',
