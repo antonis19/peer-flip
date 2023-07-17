dc04d87a52bdc43e3fbd5b808db562e232751349
antonis19 <antonis19@mail.com>
2023-07-18 01:19:40 +0300

    add navbar

--- peer-flip-frontend/src/App.tsx
+++ peer-flip-frontend/src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 import React, { useState, useMemo, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { Typography, Container, Box } from '@mui/material';
 import { UsernameInput } from './components/UsernameInput';
@@ -14,6 +14,11 @@ import CoinFlip from './components/CoinFlip';
 import { RoomContext } from './contexts/RoomContext';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useMediaQuery } from '@mui/material';
+import { AppBar, Toolbar, Button } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import Navbar from './components/Navbar';
+
 
 
 function App() {
@@ -75,7 +80,8 @@ function App() {
         <RoomContext.Provider value={{ joining, setJoining, joined, setJoined }}>
           <ErrorContext.Provider value={{ errorMessage, setErrorMessage }}>
             <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', bgcolor: '#7FFFD4' }}>
-              <Typography variant="h3" align="center" gutterBottom>
+              <Navbar></Navbar>
+              <Typography variant="h4" align="center" gutterBottom>
                 P2P Coin Flip
               </Typography>
               <ErrorAlert message={errorMessage} />
---
+++ peer-flip-frontend/src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import HomeIcon from '@mui/icons-material/Home';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import Box from '@mui/material/Box';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+
+const Navbar = () => {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+    return (
+        <AppBar position="static" style={{ backgroundColor: 'rgb(44 130 84)' }}>
+            <Toolbar disableGutters>
+                <Box display="flex" alignItems="center">
+                    <Button color="inherit" href="/" style={{ height: '64px', minHeight: '64px' }}>
+                        <HomeIcon />
+                    </Button>
+                </Box>
+                <Typography variant={isMobile ? "subtitle1" : "h6"} component="div" sx={{ flexGrow: 1 }}>
+                    PeerFlip: A P2P Coin Flip Protocol
+                </Typography>
+                <Button color="inherit" href="/how-it-works" style={{ height: '64px', minHeight: '64px' }}>How It Works</Button>
+                <Button color="inherit" href="https://github.com/antonis19/peer-flip" target="_blank" style={{ height: '64px', minHeight: '64px' }}>
+                    <GitHubIcon />
+                </Button>
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+export default Navbar;
